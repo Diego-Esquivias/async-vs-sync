e3805cd794e1bf48a7f67cbbcbf090f6f1bae291
Diego-Esquivias <desqui048@west-mec.org>
2024-08-27 15:33:06 -0700

    update

--- node.js
+++ node.js
@@ -1,3 +1,5 @@
+const {readFile, readFileSync, read} = require('fs')
+const path = require('path')
 // // Problem 1 ------------------------------------------------------------------------------------------------------------------------
 // console.log('This is synchronous')
 // setTimeout(() => {
@@ -14,21 +16,45 @@
 //     clearInterval(printMess)
 // }, 10000);
 
-// Problem 3 --------------------------------------------------------------------------------------------------------------------------
-const fs = require('fs')
+// // Problem 3 --------------------------------------------------------------------------------------------------------------------------
 
-fs.readFile('input.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-});
+// readFile(path.join(__dirname, 'input.txt'), 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(data + '|sync');
+// });
 
-fs.readFileSync('input.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-});
+// const async = readFileSync(path.join(__dirname, 'input.txt'), 'utf8', (data) => {
+//     console.log(data);
+// });
+
+// console.log(async + '|async')
+
+// // Problem 4 --------------------------------------------------------------------------------------------------------------------------
+
+// readFile(path.join(__dirname, 'data.txt'), 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data + '|sync');
+// });
+
+// setTimeout(() => {
+//   const dataResult = readFileSync(path.join(__dirname, 'asyncData.txt'), 'utf8', (data) => {
+//     console.log(data);
+//   });
+//   console.log(dataResult)
+// }, 2000);
+
+// console.log('Script complete')
+
+// Problem 5 --------------------------------------------------------------------------------------------------------------------------
+
+console.log('Countdown initiated...')
+
+const countDown = setInterval(() => {
+    console.log('Repeating message...')
+}, 1000);
